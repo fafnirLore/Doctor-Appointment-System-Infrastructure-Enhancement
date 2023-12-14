df41a51d710dc0e308fc1937ee62bc92274b9373
FF <farquleetgondal1234@gmail.com>
2023-12-14 09:47:00 +0500

    Database added

--- appointments/app.py
+++ appointments/app.py
@@ -1,27 +1,51 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
+from pymongo import MongoClient
+import os
+
 app = Flask(__name__)
+APPOINTMENTS_DB_URL = ""
+if os.environ.get("APPOINTMENTS_DB_URL"):
+    APPOINTMENTS_DB_URL = os.environ.get("APPOINTMENTS_DB_URL")
+else:
+    APPOINTMENTS_DB_URL = "localhost:27017"
+
+print(f"APPOINTMENTS_DB_URL: {APPOINTMENTS_DB_URL}")
 
 appointments = [
-  { 'id': "1",'doctor': "1", 'date': "21 Nov 2023", 'rating':"Good"  },
-  { 'id': "2",'doctor': "1", 'date': "22 Nov 2023", 'rating':"Bad"  },
-  { 'id': "3",'doctor': "2", 'date': "22 Nov 2023", 'rating':"Good"  },
-  { 'id': "4",'doctor': "1", 'date': "22 Nov 2023", 'rating':"Bad"  },
-  { 'id': "5",'doctor': "2", 'date': "22 Nov 2023", 'rating':"Good"  },
+    {'id': "1", 'doctor': "1", 'date': "21 Nov 2023", 'rating': "Good"},
+    {'id': "2", 'doctor': "1", 'date': "22 Nov 2023", 'rating': "Bad"},
+    {'id': "3", 'doctor': "2", 'date': "22 Nov 2023", 'rating': "Good"},
+    {'id': "4", 'doctor': "1", 'date': "22 Nov 2023", 'rating': "Bad"},
+    {'id': "5", 'doctor': "2", 'date': "22 Nov 2023", 'rating': "Good"}
 ]
+
+# Connect to MongoDB
+client = MongoClient(f'mongodb://{APPOINTMENTS_DB_URL}')
+db = client['Doctor-Appointment-System']
+appointments_collection = db['Appointment']
+
+# Check if the collection is empty
+if appointments_collection.count_documents({}) == 0:
+    # Insert appointment data into MongoDB
+    result = appointments_collection.insert_many(appointments)
+    print("Appointment data inserted successfully.")
+else:
+    print("Database is not empty. Appointment data not inserted.")
 
 @app.route('/hello')
 def hello():
-  greeting = "Hello world!"
-  return greeting
+    greeting = "Hello world!"
+    return greeting
 
 @app.route('/appointments', methods=["GET"])
 def getAppointments():
-  return jsonify(appointments)
+    appointments = list(appointments_collection.find( {}, {'_id': False}))
+    return jsonify(appointments)
 
 @app.route('/appointment/<id>', methods=["GET"])
 def getAppointment(id):
-  id = int(id) - 1
-  return jsonify(appointments[id])
+    appointment = appointments_collection.find_one({'id': id}, {'_id': False})
+    return jsonify(appointment)
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0",port=7070)
+    app.run(host="0.0.0.0", port=7070)
--- doctors/app.py
+++ doctors/app.py
@@ -1,24 +1,47 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
+from pymongo import MongoClient
+import os
+
+DOCTORS_DB_URL = ""
+if os.environ.get("DOCTORS_DB_URL"):
+    DOCTORS_DB_URL = os.environ.get("DOCTORS_DB_URL")
+else:
+    DOCTORS_DB_URL = "localhost:27017"
+
+print(f"DOCTORS_DB_URL: {DOCTORS_DB_URL}")
+
 app = Flask(__name__)
 
-doctors = [
-  { 'id': "1",'firstName': "Muhammad Ali", 'lastName': "Kahoot", 'speciality':"DevOps"  },
-  { 'id': "2",'firstName': "Good", 'lastName': "Doctor",'speciality':"Test"  }
-]
+# Connect to MongoDB
+client = MongoClient(f'mongodb://{DOCTORS_DB_URL}')
+db = client['Doctor-Appointment-System']
+doctors_collection = db['Doctor']
+
+# Check if the collection is empty
+if doctors_collection.count_documents({}) == 0:
+    # Insert doctors data into MongoDB
+    doctors_data = [
+        {'id': "1", 'firstName': "Muhammad Ali", 'lastName': "Kahoot", 'speciality': "DevOps"},
+        {'id': "2", 'firstName': "Good", 'lastName': "Doctor", 'speciality': "Test"}
+    ]
+    doctors_collection.insert_many(doctors_data)
+    print("Doctors data inserted successfully.")
 
 @app.route('/hello')
 def hello():
-  greeting = "Hello world!"
-  return greeting
+    greeting = "Hello world!"
+    return greeting
 
 @app.route('/doctors', methods=["GET"])
 def getDoctors():
-  return jsonify(doctors)
+    doctors = list(doctors_collection.find({}, {'_id': False}))
+    print(doctors)
+    return jsonify(doctors)
 
 @app.route('/doctor/<id>', methods=["GET"])
 def getDoctor(id):
-  id = int(id) - 1
-  return jsonify(doctors[id])
+    doctor = doctors_collection.find_one({'id': id}, {'_id': False})
+    return jsonify(doctor)
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0",port=9090)
+    app.run(host="0.0.0.0", port=9090)
